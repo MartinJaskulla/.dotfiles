a96fd8cb4169806fc670aef52f4a16b25e85ada1
Martin Jaskulla <35801656+MartinJaskulla@users.noreply.github.com>
2022-05-20 11:21:00 +0200

    Add install.sh

---
+++ .dotfiles/install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# https://www.atlassian.com/git/tutorials/dotfiles
+
+git clone --bare https://github.com/MartinJaskulla/.dotfiles.git $HOME/.dotfiles
+
+function dotfiles {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+dotfiles checkout
+if [ $? = 0 ]; then
+  echo "Checked out dotfiles.";
+  else
+    mkdir -p dotfiles-backup
+    echo "Backing up pre-existing dotfiles to file dotfiles-backup.";
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} dotfiles-backup/{}
+fi;
+dotfiles checkout
+dotfiles config status.showUntrackedFiles no
+
+which -s brew
+if [[ $? = 0 ]] ; then
+  echo "Homebrew already installed. Updating..."
+  brew update
+  brew upgrade
+else
+  echo "Homebrew not installed. Installing..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+brew install git
+brew install docker
+brew install tmux
+brew install pyenv
+brew install nvm
+
+brew install --cask google-chrome
+brew install --cask firefox
+brew install --cask webstorm
+brew install --cask visual-studio-code
+brew install --cask postman
+
+# Output Brewfile with current packages
+# brew bundle dump --describe 
+# Install Brewfile
+# brew bundle --file ~/.dotfiles/Brewfile
